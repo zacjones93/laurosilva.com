9c89725420ffdb677cf050195ff4e08de98c6cc6
Zac Jones <zacjones93@gmail.com>
2022-04-15 13:41:05 -0400

    remove unneeded min-h

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="m-auto text-center max-w-7xl sm:min-h-[60vh] relative w-full flex flex-col items-center justify-center sm:py-32 py-24 px-5">
         <h1 className="text-3xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl xl:text-7xl">
           Lauro is building{' '}
-          <span className="inline-block leading-[1.1] text-transparent min-h-fit bg-clip-text bg-gradient-to-r from-sky-500 to-blue-500 ">
+          <span className="inline-block leading-[1.1] text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-500 ">
             badass high-quality
           </span>{' '}
           learning products with friends
