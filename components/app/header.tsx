e9cdaac0ddae88eb0bb60482f673ed30a73cc973
Lauro <hey@laurosilva.com>
2022-04-15 00:16:41 -0700

    adding links

--- components/app/header.tsx
+++ components/app/header.tsx
@@ -1,22 +1,24 @@
+import Link from 'next/link'
+
 export default function Navigation() {
   return (
     <nav className="mx-auto mb-4 max-w-screen-2xl" aria-label="Top">
       <div className="flex items-center justify-between w-full py-6">
         <div className="flex items-center">
-          <a href="/">
-            <span className="sr-only">Lauro Silva</span>
-            <img className="w-auto h-10" src="/logo.svg" alt="" />
-          </a>
+          <Link href="/">
+            <a>
+              <span className="sr-only">Lauro Silva</span>
+              <img className="w-auto h-10" src="/logo.svg" alt="" />
+            </a>
+          </Link>
         </div>
         <div className="ml-10 space-x-4">
           {navigation.map(link => (
-            <a
-              key={link.name}
-              href={link.href}
-              className="text-sm font-medium text-white hover:text-indigo-50"
-            >
-              {link.name}
-            </a>
+            <Link key={link.name} href={link.href}>
+              <a className="text-sm font-medium text-white hover:text-indigo-50">
+                {link.name}
+              </a>
+            </Link>
           ))}
         </div>
       </div>
