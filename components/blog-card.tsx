725296cf1a17182c0e439eff4f819f735859e802
Lauro <hey@laurosilva.com>
2022-04-15 01:51:10 -0700

    update

--- components/blog-card.tsx
+++ components/blog-card.tsx
@@ -14,7 +14,6 @@ export default function BlogPost({
           <h3 className="text-2xl font-extrabold tracking-tight leading-[1.1] sm:text-2xl lg:text-3xl xl:text-3xl mb-6">
             {title}
           </h3>
-
           <p className="text-gray-300">{summary}</p>
         </a>
       </div>
